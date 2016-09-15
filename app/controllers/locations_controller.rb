a1cdc781298ec6da40a4ac624b4138ca00f53ba4
Jason Hanna <jason.hanna01@gmail.com>
2016-09-15 14:56:52 -0600

    fixing merge conflicts

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,7 +5,6 @@ class LocationsController < ApplicationController
       @location = Location.search_by_current_location
     else
       @location = Location.search_by_input_location(params[:q])
-      # require "pry"; binding.pry
     end
   end
 
