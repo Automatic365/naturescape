e5021c5ebefa9e43e3f6a703408108366178a3c4
Jason Hanna <jason.hanna01@gmail.com>
2016-09-15 08:57:26 -0600

    add accurate display of city, state, zip on location show page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,7 +2,7 @@ class LocationsController < ApplicationController
 
   def show
     @location = Location.search(params[:q])
-    require "pry"; binding.pry
+    # require "pry"; binding.pry
   end
 
 end
