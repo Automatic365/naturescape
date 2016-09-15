3a4db3e32ac477f6d9799cd70415d24b108395f1
Jason Hanna <jason.hanna01@gmail.com>
2016-09-15 13:37:32 -0600

    fix oauth error in sessions controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,7 +2,20 @@ class LocationsController < ApplicationController
 
   def show
     @location = Location.search(params[:q])
+    # @location = LocationPresenter.search(params[:q])
     # require "pry"; binding.pry
   end
 
 end
+
+# class LocationPresenter
+#   def self.search(params)
+#     if params == #to a zip
+#      @location = Location.search(params)
+#      @location.parse_for_zip
+#    else
+#      @location = Location.search(params)
+#      @location.parse_for_address
+#    end
+#  end
+# end
