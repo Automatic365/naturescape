a484aad9490564b734bf054e096082cd6733ff74
Jason Hanna <jason.hanna01@gmail.com>
2016-09-17 12:54:45 -0600

    add google maps to location show page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,6 +5,7 @@ class LocationsController < ApplicationController
       @location = Location.search_by_current_location
     else
       @location = Location.search_by_input_location(params[:q])
+      # require "pry"; binding.pry
     end
   end
 
---
+++ app/models/states.rb
@@ -0,0 +1,13 @@
+class States
+
+  def self.states_list
+    ["Alaska","Arizona", "Arkansas", "California", "Colorado", "Florida",
+    "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
+    "Louisiana", "Maryland", "Massachusetts", "Michigan", "Minnesota",
+    "Montana", "Nebraska", "New Mexico", "New York", "North Carolina", "Ohio",
+    "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+    "Tennessee", "Texas", "Utah", "Virginia", "Washington", "West Virginia",
+     "Wisconsin", "Wyoming"]
+  end
+
+end
