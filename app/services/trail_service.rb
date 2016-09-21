375dc79046032e17d8d08d9d5819616e410d68b9
Jason Hanna <jason.hanna01@gmail.com>
2016-09-21 09:54:35 -0600

    closes #18 (#19)
    * Development (#16)
    
    * convert geocode output to openstruct
    
    * add accurate display of city, state, zip on location show page
    
    * add simplecov
    
    * oauth test passing
    
    * fix oauth error in sessions controller
    
    * replace geocoder with google geocoder (#9)
    
    * replace geocoder with google geocoder
    - change views to accommodate proper display info
    
    * remove comments
    
    * welcome page functionality complete (#10)
    
    * add excuses page
    
    * add google maps to location show page
    
    * add location show page (#12)
    
    * add dropdown boxes that pass along accurate info on welcome page and trail index page
    
    * add current location button to welcome page. needs refactoring
    
    * add styling to form and buttons (#15)
    
    * style search current location button
    
    * add alternative activity index view
    
    * add button and form styling
    
    * rename app in browser
    
    * reformatted trail location menus to remove trails without location info

--- app/services/trail_service.rb
+++ app/services/trail_service.rb
@@ -18,24 +18,43 @@ class TrailService
 
   def formatted_locations_by_state(state)
     locations = hiking_locations_by_state(state)
-    trails = locations.places.map do |location|
-      location.name
-    end
-    cities = locations.places.map do |location|
-      location.city
-    end
-    results = trails.zip(cities)
-    formatted_result = results.map do |result|
-      result.compact
-    end
-     formatted_result
+    scrubbed_locations = scrub_results(locations)
+    names = get_trail_names(scrubbed_locations)
+    cities = get_trail_cities(scrubbed_locations)
+    get_formatted_result(names, cities)
   end
 
 
 
+private
 
   def parse(response)
     JSON.parse(response, object_class: OpenStruct)
   end
 
+  def get_trail_names(locations)
+    locations.map do |location|
+      location.name
+    end
+  end
+
+  def get_trail_cities(locations)
+    locations.map do |location|
+      location.city
+    end
+  end
+
+  def get_formatted_result(names, cities)
+    results = names.zip(cities)
+    results.map do |result|
+      result.compact
+    end
+  end
+
+  def scrub_results(locations)
+    locations.places.reject do |location|
+      location.lat == 0.0
+    end
+  end
+  #
 end
